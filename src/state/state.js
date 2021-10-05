16b2adceb7306b7c6e7d15916b0f81680d5134ef
benedictussteven <49013736+benedictussteven@users.noreply.github.com>
2021-10-05 22:42:08 +0700

    .....
    .....

--- src/state/state.js
+++ src/state/state.js
@@ -66,20 +66,6 @@ export default function Index() {
         return (
           <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
             {children}
-            {/* {onClose ? (
-              <IconButton
-                aria-label="close"
-                onClick={onClose}
-                sx={{
-                  position: 'absolute',
-                  right: 8,
-                  top: 8,
-                  color: (theme) => theme.palette.grey[500],
-                }}
-              >
-                <CloseIcon />
-              </IconButton>
-            ) : null} */}
           </DialogTitle>
         );
       };
